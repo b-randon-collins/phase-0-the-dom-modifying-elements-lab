fd2a5dc6da8c5c2a49cfeb421401ce1b4c31bd73
Brandon Collins <brandonc503@yahoo.com>
2024-07-11 18:31:31 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const mainNode = document.querySelector("#main");
+if (mainNode) {
+  mainNode.remove();
+}
+const newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.textContent = "Brandon Collins is the champion";
